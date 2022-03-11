fd3b9ae4fb94d3eb00e8a3f8b9eb77641a648549
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2022-03-11 21:06:35 +0100

    1.0.0

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+   'env': {
+      'browser': true,
+      'es2021': true,
+      'node': true,
+   },
+   'extends': [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended'
+   ],
+   'parser': '@typescript-eslint/parser',
+   'parserOptions': {
+      'ecmaVersion': 'latest',
+      'sourceType': 'module'
+   },
+   'plugins': [
+      '@typescript-eslint'
+   ],
+   'ignorePatterns': ['dist/', '*.d.ts'],
+   'rules': {
+      'indent': [
+         'error',
+         3,
+         {
+            'SwitchCase': 1
+         }
+      ],
+      'linebreak-style': [
+         'error',
+         'unix'
+      ],
+      'quotes': [
+         'error',
+         'single'
+      ],
+      'semi': [
+         'error',
+         'always'
+      ],
+      'no-trailing-spaces': [
+         'warn',
+         {
+            'ignoreComments': true
+         }
+      ],
+      '@typescript-eslint/no-unused-vars': [
+         'warn',
+         {
+            'argsIgnorePattern': '^_'
+         }
+      ],
+   }
+};
